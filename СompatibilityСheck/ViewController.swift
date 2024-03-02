d7991724e72bcca4fae418a83d5c3969eefa14e4
Bobbi R <bobbi@Noutbuk-Bobbi.local>
2024-03-02 13:34:37 +0300

    initial project

---
+++ СompatibilityСheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  СompatibilityСheck
+//
+//  Created by Bobbi R. on 2.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
