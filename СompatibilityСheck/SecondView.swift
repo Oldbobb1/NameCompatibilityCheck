71cb67ad1a29642895bf9fb7ecfcf7dee2c41030
Bobbi R <bobbi@Noutbuk-Bobbi.local>
2024-03-02 14:15:52 +0300

    Three functions are declared, one for the button two for counting and outputting the result.

--- СompatibilityСheck/SecondView.swift
+++ СompatibilityСheck/SecondView.swift
@@ -9,8 +9,8 @@ import SnapKit
 
 class SecondViewController: UIViewController{
     
-    private var firstName: String!
-    private var secondName: String!
+     var firstName: String!
+     var secondName: String!
     
     private var resultValue = 0
     
@@ -62,12 +62,23 @@ class SecondViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        resultValue = findResult()
+        
+        secondLabel.text = "\(firstName ?? "" ) and \(secondName ?? "" )"
+        thirdLabel.text = resultValue.formatted(.percent)
+
+        progress.progress = Float(resultValue) / 100
+        
         view.backgroundColor = .white
+        
         view.addSubview(firstLabel)
         view.addSubview(secondLabel)
         view.addSubview(progress)
         view.addSubview(buttonCreat)
         view.addSubview(thirdLabel)
+        
+        buttonCreat.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        
         
         firstLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(100)
@@ -98,4 +109,65 @@ class SecondViewController: UIViewController{
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-140)
         }
     }
+    
+    @objc  func closeButtonTapped() {
+        
+        UIView.animate(withDuration: 0.1) {
+            self.view.frame.origin.y = self.view.frame.height
+        } completion: { _ in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
+                self.presentingViewController?.dismiss(animated: true)
+            }
+            self.removeFromParent()
+            self.view.removeFromSuperview()
+        }
+    }
+    
+    private func findValue(for name: String) -> Int {
+        var count = 0
+        for character in name.lowercased() {
+            switch character {
+            case "a","i","j","q","y":
+                count += 1
+            case "b","k","r":
+                count += 2
+            case "c","g","l","s":
+                count += 3
+            case "d","m","t":
+                count += 4
+            case "e","h","n","x":
+                count += 5
+            case "u","v","w":
+                count += 6
+            case "o","z":
+                count += 7
+            case "f","p":
+                count += 8
+            default:
+                count += 0
+            }
+        }
+        return count
+    }
+    
+    private func findResult() -> Int {
+        var result = 0
+        let firstValue = findValue(for: firstName)
+        let secondValue = findValue(for: secondName)
+        let absDifference = abs (firstValue - secondValue)
+        switch absDifference {
+        case 0, 1, 2:
+            result = 100
+        case 3, 4:
+            result = 65
+        case 5:
+            result = 50
+        case 6:
+            result = 40
+        default:
+            result = 30
+        }
+        return result
+    }
 }
+
